7b5141b566f9dac3c2e1b18a880b3e0016018e7c
jmontes <jhon.montes.mt@usa.edu.co>
2022-10-18 14:15:18 -0500

    reto4 terminado

--- src/main/java/proyecto/ciclo3/gym/controllers/AdminController.java
+++ src/main/java/proyecto/ciclo3/gym/controllers/AdminController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -46,6 +48,22 @@ public class AdminController {
     public Admin saveAdmin(@RequestBody Admin ad) {
 
         return adminService.adminGuardar(ad);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Admin update(@RequestBody Admin ad) {
+
+        return adminService.ActualizarAdmin(ad);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int id) {
+
+        return adminService.eliminarAdmin(id);
+
     }
 
 }
